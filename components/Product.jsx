75507a17a40382db3d27abba82943ee314ed0aab
Juan Brizuela <juanbrizuelago@gmail.com>
2022-10-22 16:51:27 -0500

    added key

--- components/Product.jsx
+++ components/Product.jsx
@@ -13,7 +13,7 @@ const Product = ({ products }) => {
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <a
-              key={product.id}
+              key={product._id}
               href={`/product/${product.slug.current}`}
               className="group"
             >
