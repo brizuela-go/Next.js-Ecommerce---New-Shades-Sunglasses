cfb7aaa790d0e346587083bdd589470c8ad3090e
Juan Brizuela <juanbrizuelago@gmail.com>
2022-10-22 17:30:04 -0500

    added next link

--- components/Product.jsx
+++ components/Product.jsx
@@ -12,24 +12,22 @@ const Product = ({ products }) => {
 
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a
-              key={product._id}
-              href={`/product/${product.slug.current}`}
-              className="group"
-            >
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-newshadesbox xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  loading="lazy"
-                  src={urlFor(product.image[0])}
-                  alt={product.name}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
-            </a>
+            <Link href={`/product/${product.slug.current}`}>
+              <a key={product._id} className="group">
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-newshadesbox xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    loading="lazy"
+                    src={urlFor(product.image[0])}
+                    alt={product.name}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </a>
+            </Link>
           ))}
         </div>
       </div>
