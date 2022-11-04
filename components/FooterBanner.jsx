6d1db130ae35303674b4c7f7617ce250c4b1dda3
Juan Brizuela <juanbrizuelago@gmail.com>
2022-11-03 18:30:16 -0600

    commit

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -18,7 +18,7 @@ const FooterBanner = ({
   },
 }) => {
   return (
-    <div className="bg-red-600  rounded-2xl dark:bg-blue-300">
+    <div className="bg-red-600  rounded-2xl ">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
           <span className="block">¿Resides en la Ciudad de Puebla?</span>
